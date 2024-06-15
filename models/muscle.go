76562266061c0ce2306f5612d805795714abb5b8
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-15 06:10:27 +0300

    refactor: Remove unused many-to-many relationships in Muscle model

--- models/muscle.go
+++ models/muscle.go
@@ -7,6 +7,4 @@ import (
 type Muscle struct {
 	gorm.Model
 	Name       string       `json:"name"`
-	Exercises  []Exercise   `gorm:"many2many:exercise_muscles;"`
-	Exercises2 []Exercise   `gorm:"many2many:exercise_additional_muscles;"`
 }
