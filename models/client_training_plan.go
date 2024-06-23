702c367c2cc431cebb8079c167d259d3b6f60d4d
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-24 02:24:54 +0500

    refactor: Separate exercise units data loading in load_data.go

--- models/client_training_plan.go
+++ models/client_training_plan.go
@@ -8,11 +8,11 @@ import (
 
 type ClientTrainingPlan struct {
     gorm.Model
-    UserID             uint            `json:"user_id"`
-    TrainingPlanID     uint            `json:"training_plan_id"`
     Name               string          `json:"name"`
 	Description        string          `json:"description"`
     Workouts           []ClientWorkout `gorm:"foreignkey:ClientTrainingPlanID"`
+    UserID             uint            `json:"user_id"`
+    TrainingPlanID     uint            `json:"training_plan_id"`
     StartDate          *time.Time      `json:"start_date"`
     EndDate            *time.Time      `json:"end_date"`
     PlannedStartDate   *time.Time      `json:"planned_start_date"`
