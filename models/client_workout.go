b7fcb199ae55bc46119abc2a05a8700e3876fea8
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-24 03:13:26 +0500

    refactor: Update ClientWorkout model to include DaysUntilNext
    This code change in the `models/client_workout.go` file updates the `ClientWorkout` model to include a new field `DaysUntilNext`. This field represents the number of days until the next workout for the client. The `DaysUntilNext` field is added after the `Description` field and before the `Order` field. This refactor enhances the functionality of the application by providing a way to track and display the time until the next workout for clients.

--- models/client_workout.go
+++ models/client_workout.go
@@ -9,11 +9,12 @@ import (
 type ClientWorkout struct {
     gorm.Model
     ClientTrainingPlanID uint                    `json:"client_training_plan_id"`
-    WorkoutID            uint                    `json:"workout_id"`
     Name                 string                  `json:"name"`
 	Description          string                  `json:"description"`
-    Exercises            []ClientWorkoutExercise `gorm:"foreignkey:ClientWorkoutID"`
+    DaysUntilNext  int               `json:"days_until_next"`
     Order                int                     `json:"order"`
+    Exercises            []ClientWorkoutExercise `gorm:"foreignkey:ClientWorkoutID"`
+    WorkoutID            uint                    `json:"workout_id"`
     StartDate            *time.Time              `json:"start_date"`
     EndDate              *time.Time              `json:"end_date"`
     PlannedStartDate     *time.Time              `json:"planned_start_date"`
